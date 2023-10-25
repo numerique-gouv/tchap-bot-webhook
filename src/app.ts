d6b8d5c3b2793e481bfacdbe60334092a9030511
Benoit Serrano <benoit.serrano10@gmail.com>
2023-10-25 12:41:22 +0200

    delete client

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import Express, { Response } from 'express';
 import bodyParser from 'body-parser';
-import path from 'path';
 import { buildRouter } from './router';
 import { config } from './config';
 import { dataSource } from './dataSource';
@@ -19,12 +18,6 @@ async function runApp() {
     const router = await buildRouter(dataSource);
 
     app.use('/api', bodyParser.json(), router);
-
-    app.use(Express.static(path.join(__dirname, '..', 'src', 'client', 'build')));
-
-    app.get('*', (_, res: Response) => {
-        res.sendFile(path.join(__dirname, '..', 'src', 'client', 'build', 'index.html'));
-    });
 
     app.listen(config.PORT, async () => {
         console.log(`Server is running on port ${config.PORT}`);
--- src/client/src/pages/index.ts
+++
@@ -1,4 +0,0 @@
-import { AddUrl } from './AddUrl';
-import { FAQ } from './FAQ';
-
-export { AddUrl, FAQ };
